3601515312c96848bdb8f405e06df93d2b72c5a0
MarwahAdnan <marwah.designer@gmail.com>
2024-07-11 09:36:00 +0300

    Add files via upload

---
+++ src/views/tlkhes.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+  
+        <div class=" flex m-0 p-0  overflow-x-hidden bg-[#ECECF3]">
+
+      
+
+            <SidBar  class=" w-1/5"></SidBar>
+
+          
+        
+        
+        <div class=" w-4/5">
+            
+            <NavBarVisitore></NavBarVisitore>
+
+            <div class=" text-ms text-start mx-10 text-[#503E9D] my-3" >
+                الصفحة  الرئيسية &gt;  خدمات معين &gt; معين 
+</div>
+
+</div>
+</div>
+</div>
+</template>
+
+<script>
+
+import NavBarVisitore from '../components/NavBarVisitore'
+import SidBar from '../components/SidBar.vue'
+export default {
+    name:'TalkhesVue',
+    components:{
+        NavBarVisitore,
+        SidBar,
+    },}
+    </script>
